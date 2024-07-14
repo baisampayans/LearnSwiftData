9a06c43d4691a4dc1d9a0b7607f29fdb6ba1294b
Baisampayan <baisampayans@gmail.com>
2024-07-14 13:54:47 +0530

    Initial Commit

---
+++ LearnSwiftData/LearnSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftDataApp.swift
+//  LearnSwiftData
+//
+//  Created by baisampayan saha on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
